fdb728d50f203b298ead625669bf2407cd6091c8
CyberSorcerer37 <krzysztofkasperczyk923@gmail.com>
2025-06-10 20:18:06 +0200

    Create CV section update
    -Created CV model
    -Created CvController
    -Created CV view

---
+++ CV_Generator/Controllers/CvController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CvGenerator.Models;
+
+namespace CvGenerator.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: /Cv/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            // Prepare an empty model for the view
+            var model = new CurriculumVitae
+            {
+                Personal = new PersonalInfo(),
+                Education = new List<EducationItem> { new EducationItem() },
+                Experience = new List<WorkExperience> { new WorkExperience() },
+                Skills = new List<Skill> { new Skill() }
+            };
+
+            return View(model);
+        }
+
+        // POST: /Cv/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CurriculumVitae model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // For now, redirect to preview
+            return RedirectToAction("Preview", model);
+        }
+
+        // GET: /Cv/Preview
+        [HttpGet]
+        public IActionResult Preview(CurriculumVitae model)
+        {
+            return View(model);
+        }
+    }
+}
