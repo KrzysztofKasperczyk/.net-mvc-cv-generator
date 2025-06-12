f83e04031d0eaabf271c6eb037d5c2e71a83b9e9
CyberSorcerer37 <krzysztofkasperczyk505@gmail.com>
2025-06-12 14:15:03 +0200

    Personal Information section update
    - Textboxes now altert the user when input is incorrect
    - Email address max 100 characters
    - FullName max 100 characters, only letters and spaces
    - Implemented country code for phone numer
    - Phone number now in: xxx-xxx-xxx pattern automatically

--- CV_Generator/Controllers/CvController.cs
+++ CV_Generator/Controllers/CvController.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using CvGenerator.Models;
 
 namespace CvGenerator.Controllers
 {
     public class CvController : Controller
     {
+        private List<SelectListItem> GetCountryCodes()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem { Text = "PL +48", Value = "+48" },
+                new SelectListItem { Text = "US +1", Value = "+1" },
+                new SelectListItem { Text = "UK +44", Value = "+44" }
+            };
+        }
+
         // GET: /Cv/Create
         [HttpGet]
         public IActionResult Create()
         {
+            ViewBag.CountryCodes = GetCountryCodes();
             var model = new CurriculumVitae
             {
-                Personal = new PersonalInfo(),
+                Personal = new PersonalInfo { CountryCode = "+48" },
                 Education = new List<EducationItem> { new EducationItem() },
                 Experience = new List<WorkExperience> { new WorkExperience() },
                 Skills = new List<Skill> { new Skill() }
@@ -26,6 +38,7 @@ namespace CvGenerator.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(CurriculumVitae model)
         {
+            ViewBag.CountryCodes = GetCountryCodes();
             // For preview purposes, bypass validation and directly render the Preview view
             return View("Preview", model);
         }
